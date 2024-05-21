18066a1ada07b949e5b791455963c41e6f3c7b67
Matheus-Moura26 <moura.matheus26@gmail.com>
2024-05-20 22:14:07 -0300

    pagina de produtos criada e adicionado headMenu

---
+++ src/pages/produtos/produtos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeadMenu from "../../Components/visualComponents/estructureComponents/headMenu/headMenu";
+
+
+export default function Produtos(){
+
+
+    return(
+        <HeadMenu />
+
+    )
+}
